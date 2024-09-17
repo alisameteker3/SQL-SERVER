26d09c3737512fe15eb692150ef839fa3b8953b0
Ali Samet Eker <146390419+alisameteker3@users.noreply.github.com>
2024-09-17 18:49:58 +0300

    Add files via upload

---
+++ selectwherefromquery.sql
@@ -0,0 +1,7 @@
+SELECT * FROM Ogrenci WHERE ErkekMi = 1 -- erkek veriler		
+
+SELECT * FROM Ogrenci WHERE ErkekMi = 0 -- kadýn veriler
+
+SELECT * FROM Ogrenci WHERE Ad = 'ALÝ' -- adý ali olanlarý görüntüler
+
+SELECT * FROM Ogrenci WHERE KayitTarihi >= '20220102'-- tarihten büyük verileri verir
