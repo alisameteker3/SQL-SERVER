a426012fcc8babe8685f6c731e40c897c5536fe7
Ali Samet Eker <146390419+alisameteker3@users.noreply.github.com>
2024-09-18 16:27:18 +0300

    Add files via upload

---
+++ insertintoSQLQuery.sql
@@ -0,0 +1,4 @@
+insert into Ogrenci values (925,'batu','Alp','25896314789','20220101',1,'2000-01-09',70,0,0)
+
+insert into Ogrenci (OgrenciNo,Ad,Soyad,TCKimlikNo,KayitTarihi,ErkekMi,DogumTarihi)
+			values(980,'Damla','Acar','30126547896','2021-01-02',0,'20000505')
