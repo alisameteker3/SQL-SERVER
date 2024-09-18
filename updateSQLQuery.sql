779a380eec744c1660a1120bbe74c138b16a8671
Ali Samet Eker <146390419+alisameteker3@users.noreply.github.com>
2024-09-18 16:27:25 +0300

    Add files via upload

---
+++ updateSQLQuery.sql
@@ -0,0 +1,5 @@
+update Ogrenci set SinavNotu1 = 75 where Ad = 'Damla' and Soyad = 'Acar'
+
+update Ogrenci set SinavNotu2 = 78 where ErkekMi = 1 
+
+update Ogrenci set SinavNotu2 = 85 , FinalNotu = 87 where	ErkekMi = 0 and OgrenciNo >=900
