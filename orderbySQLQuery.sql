880f9a0bdaba94b90130aec848df98eecdb29cb0
Ali Samet Eker <146390419+alisameteker3@users.noreply.github.com>
2024-09-19 15:39:12 +0300

    Add files via upload

---
+++ orderbySQLQuery.sql
@@ -0,0 +1,16 @@
+SELECT * FROM Ogrenci 
+
+SELECT * FROm Ogrenci order by OgrenciNo asc -- artarak sýrala demektir
+
+SELECT * FROM Ogrenci order by Ad
+
+SELECT * FROM Ogrenci order by Soyad
+
+SELECT * FROM Ogrenci order by TCKimlikNo desc -- azalarak sýralama yap demektir
+
+SELECT * FROM Ogrenci order by KayitTarihi
+
+SELECT * FROM Ogrenci order by ErkekMi
+
+
+SELECT * FROM Ogrenci order by ErkekMi desc ,Ad,KayitTarihi desc
