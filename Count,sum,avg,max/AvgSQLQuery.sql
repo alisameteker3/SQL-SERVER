d68321ca5aa7266dc6599eb753f4236251fb329d
Ali Samet Eker <146390419+alisameteker3@users.noreply.github.com>
2024-09-19 19:47:18 +0300

    Add files via upload

---
+++ Count,sum,avg,max/AvgSQLQuery.sql
@@ -0,0 +1,12 @@
+SELECT * FROM Ogrenci
+
+--avg ortalama alma iþlemi
+
+select AVG(SinavNotu2) from	Ogrenci
+
+SELECT AVG(FinalNotu) from Ogrenci
+
+SELECT AVG(FinalNotu) from Ogrenci where FinalNotu >=50 and FinalNotu <=90
+
+
+
---
+++ Count,sum,avg,max/MaxMinSQLQuery.sql
@@ -0,0 +1,9 @@
+SELECT * FROM Ogrenci
+
+-- max
+
+SELECT min(SinavNotu2) from Ogrenci
+
+select max(SinavNotu1) from Ogrenci where SinavNotu1 between 50 and 78
+
+
