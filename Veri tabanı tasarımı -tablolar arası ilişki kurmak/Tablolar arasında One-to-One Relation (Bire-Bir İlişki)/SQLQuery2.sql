3946fa90a60dac3dd2f8ee5c62b673bb5e24d77c
Ali Samet Eker <146390419+alisameteker3@users.noreply.github.com>
2024-10-21 00:46:35 +0300

    Add files via upload

---
+++ Veri tabanı tasarımı -tablolar arası ilişki kurmak/Tablolar arasında One-to-One Relation (Bire-Bir İlişki)/SQLQuery2.sql
@@ -0,0 +1,11 @@
+insert into Dolap (Lokasyon)values ('B blok zemin kat')
+
+insert into Dolap (Lokasyon) values ('B blok 1. kat')
+
+insert into Ogrenci2(OgrenciAdi,OgrenciSoyadi,DolapID) 
+		values('Veli','Özkan',1001)
+
+select * from Ogrenci2
+
+ 
+ 
